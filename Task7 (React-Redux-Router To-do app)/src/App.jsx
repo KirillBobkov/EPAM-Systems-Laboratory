2cc11cb020f70cf55a001d779bed72c537e0c69c
KirillBobkov <kirill.bobkov.1995@gmail.com>
2020-06-13 19:58:11 +0400

    added provider and connectedRouter in App.jsx

--- Task7 (React-Redux-Router To-do app)/src/App.jsx
+++ Task7 (React-Redux-Router To-do app)/src/App.jsx
@@ -5,7 +5,25 @@ import { Route, Switch, Redirect } from 'react-router';
 import EditPage from './pages/EditPage';
 import { LangContext, lang } from './lang-context/lang-context';
 
+import { Provider } from 'react-redux';
+import { createBrowserHistory } from 'history';
+import { applyMiddleware, createStore } from 'redux';
+import { routerMiddleware, ConnectedRouter } from 'connected-react-router';
+import createRootReducer from './store/rootReduser';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 export default class App extends Component {
+  history = createBrowserHistory();
+  store = createStore(
+    createRootReducer(this.history),
+    undefined,
+    composeWithDevTools(
+      applyMiddleware(
+        routerMiddleware(this.history)
+      )
+    )
+  );
+
   toggleLang = () => {
     this.setState(state => ({
       lang:
@@ -22,15 +40,19 @@ export default class App extends Component {
 
   render() {
     return (
-      <LangContext.Provider value={this.state}>
-        <Switch>
-          <Route path='/edit/:id/:item' component={EditPage} />
-          <Route path='/main' component={TodoPage} />
-          <Route path=''>
-            <Redirect to='/main' />
-          </Route>
-        </Switch>
-      </LangContext.Provider>
+      <Provider store={this.store}>
+        <ConnectedRouter history={this.history}>
+          <LangContext.Provider value={this.state}>
+            <Switch>
+              <Route path='/edit/:id/:item' component={EditPage} />
+              <Route path='/main' component={TodoPage} />
+              <Route path=''>
+                <Redirect to='/main' />
+              </Route>
+            </Switch>
+          </LangContext.Provider>
+        </ConnectedRouter>
+      </Provider>
     );
   }
 }
--- Task7 (React-Redux-Router To-do app)/src/index.js
+++ Task7 (React-Redux-Router To-do app)/src/index.js
@@ -1,29 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import App from './App';
-import { createBrowserHistory } from 'history';
-import { applyMiddleware, createStore } from 'redux';
-import { routerMiddleware, ConnectedRouter } from 'connected-react-router';
-import createRootReducer from './store/rootReduser';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-const history = createBrowserHistory();
-
-const store = createStore(
-  createRootReducer(history),
-  undefined,
-  composeWithDevTools(
-    applyMiddleware(
-      routerMiddleware(history)
-    )
-  )
-);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
-  </Provider>,
+  <App />,
   document.getElementById('root'));
